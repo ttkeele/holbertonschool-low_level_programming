1b607f180cb383aa68cb0abd36a7836fbb6a3926
ttkeele <3264@holbertonschool.com>
2021-06-24 00:35:32 +0000

    this prints Promgramming is like building a multilingual puzzle followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
-#!/bin/bash
 #include <stdio.h>
-
-int puts(const char * "Programming is like building a multilingual puzzle)
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+    }
