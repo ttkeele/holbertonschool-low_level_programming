97e532290d1e919b2cfbf2b2142fc66261864bb5
ttkeele <3264@holbertonschool.com>
2021-06-25 19:32:43 +0000

    this displays the last digit of a number and if it is greater than or equal to zero but less than 6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	}
 	if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 but and not 0\n",
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 		       n, n % 10);
 	}
 	if (n % 10 == 0)
