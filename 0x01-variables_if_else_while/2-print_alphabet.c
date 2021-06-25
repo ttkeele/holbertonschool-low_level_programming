966a035ee4edc5b5726e28dd7cafbf3493a8482d
ttkeele <3264@holbertonschool.com>
2021-06-25 20:25:46 +0000

    this displays the alphbet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - displays the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
