b8ea3b5571d40d8e1e84cbf07417b50542cbc83e
ttkeele <3264@holbertonschool.com>
2021-06-25 17:58:57 +0000

    this will display a random number and declare if its positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - display if a randfom numver is positive or negative
+ *
+ * Return: 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+return (0);
+}
