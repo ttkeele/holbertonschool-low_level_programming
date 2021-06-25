cd01db86223cc3906af06f08b0abaf934a15148d
ttkeele <3264@holbertonschool.com>
2021-06-25 21:24:26 +0000

    this displays the alphabet in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - displays the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char ch = 'a';
+	char CH = 'A';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	for (CH = 'A'; CH <= 'Z'; ch++)
+
+	return (0);
+}
