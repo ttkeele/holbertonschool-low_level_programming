0f0dd5a320dd8155da3416e37d34f8d3e9fb3bdc
ttkeele <3264@holbertonschool.com>
2021-06-26 03:07:38 +0000

    this displays all single digit base 10 numbers starting with 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - displays all single digit numbers of base 10 starting from 0.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = '0';
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+
+	printf("\n");
+
+	return (0);
+}
