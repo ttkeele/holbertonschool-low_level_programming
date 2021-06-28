fd9417d0f67798b79c063b2ba8a5d3af0dadb4aa
ttkeele <3264@holbertonschool.com>
2021-06-28 20:13:47 +0000

    this displays the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * prints the alphabet in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
