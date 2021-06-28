b2f765810dfcacbb0d9056aa9fc7b14038c1b1b4
ttkeele <3264@holbertonschool.com>
2021-06-28 23:08:42 +0000

    adds to integers and displays the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ * @num1: first integer to be added
+ * @num2: second integer to be added
+ *
+ * Return: the sum
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
