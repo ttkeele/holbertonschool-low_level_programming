47099d585df030aa57fb1c57b192a5199d112e93
ttkeele <3264@holbertonschool.com>
2021-06-28 13:33:00 +0000

    this displays HOlberton with new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+/**
+ * main - displays Holberton followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('r');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+
+	return (0);
+}
