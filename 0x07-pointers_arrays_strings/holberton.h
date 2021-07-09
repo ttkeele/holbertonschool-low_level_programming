2231afdfc97956906f6aed31e1b77cc9472a318b
ttkeele <3264@holbertonschool.com>
2021-07-09 21:41:33 +0000

    this is the header file used for this directory

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,5 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stddef.h>
+
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
