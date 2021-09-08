75de0fe2f8bcda02ebf3def63e3148de1f032f3a
ttkeele <3264@holbertonschool.com>
2021-09-08 14:41:16 +0000

    this is the header file for doubly linked list

--- 0x18-doubly_linked_lists/list.h
+++ 0x18-doubly_linked_lists/list.h
@@ -9,6 +9,8 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
+ * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
